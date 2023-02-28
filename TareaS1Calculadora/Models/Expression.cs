7951b33619133aec9137a8347edc4c9d77eb693a
Francisco <109773582+franciscoflaf86v2@users.noreply.github.com>
2023-02-28 17:17:51 -0600

    commit inicial
    app de tarea semanal

---
+++ TareaS1Calculadora/Models/Expression.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaS1Calculadora.Models
+{
+    public class Expression
+    {
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        public double Sumar()
+        {
+            return Num1 + Num2;
+        }
+
+        public double Restar()
+        {
+            return Num1 - Num2;
+        }
+
+        public double Multiplicar()
+        {
+            return Num1 * Num2;
+        }
+
+        public double Dividir()
+        {
+            if (Num2 == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return Num1 / Num2;
+        }
+    }
+}
---
+++ TareaS1Calculadora/ViewModelExpression/ViewModelExpression .cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using TareaS1Calculadora.Models;
+using Xamarin.Forms;
+
+namespace TareaS1Calculadora.ViewModelExpression
+{
+    public class ViewModelExpression : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Expression _expression;
+        public Expression Expression
+        {
+            get { return _expression; }
+            set
+            {
+                _expression = value;
+                RaisePropertyChanged(nameof(Expression));
+            }
+        }
+
+        private double _resultado;
+        public double Resultado
+        {
+            get { return _resultado; }
+            set
+            {
+                _resultado = value;
+                RaisePropertyChanged(nameof(Resultado));
+            }
+        }
+
+        public ICommand CalcularCommand => new Command<string>(Calcular);
+
+        public ViewModelExpression()
+        {
+            Expression = new Expression();
+        }
+
+        public void Calcular(string operacion)
+        {
+            try
+            {
+                switch (operacion)
+                {
+                    case "Sumar":
+                        Resultado = Expression.Sumar();
+                        break;
+                    case "Restar":
+                        Resultado = Expression.Restar();
+                        break;
+                    case "Multiplicar":
+                        Resultado = Expression.Multiplicar();
+                        break;
+                    case "Dividir":
+                        Resultado = Expression.Dividir();
+                        break;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Resultado = Double.NaN;
+            }
+        }
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
